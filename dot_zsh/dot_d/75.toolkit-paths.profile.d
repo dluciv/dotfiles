1830c39088dbebbf3649297d1ea5d4c52927e3b3
Dmitry V. Luciv <dluciv@dluciv.name>
2024-12-05 15:08:05 +0300

    basher

--- dot_zsh/dot_d/75.toolkit-paths.profile.d
+++ dot_zsh/dot_d/75.toolkit-paths.profile.d
@@ -2,10 +2,17 @@ toolkit_paths=(
   ~/.dotnet/tools
   ~/go/bin
   ~/.gem/ruby/3.2.0/bin
+  ~/.basher/bin
 )
 
 for tp in $toolkit_paths; do
   if [[ -d $tp ]]; then
     path_suffix+=( $tp )
+    case $tp in
+      *basher/bin)
+        export PATH=$PATH:$tp
+        eval $(basher init - zsh)
+      ;;
+    esac
   fi
 done
