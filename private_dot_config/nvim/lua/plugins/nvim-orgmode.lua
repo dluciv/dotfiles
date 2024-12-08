a4d6812f78a92be4cff65de593ac485c27f4dfb0
Dmitry V. Luciv <dluciv@dluciv.name>
2024-12-08 22:58:45 +0300

    bullets & telescope

--- private_dot_config/nvim/lua/plugins/nvim-orgmode.lua
+++ private_dot_config/nvim/lua/plugins/nvim-orgmode.lua
@@ -5,5 +5,24 @@ return {
     config = function()
             require('orgmode').setup{}
     end
-    }
+  },
+  {'akinsho/org-bullets.nvim',
+    config = function()
+      require('org-bullets').setup()
+    end
+  },
+  {'nvim-orgmode/telescope-orgmode.nvim',
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-orgmode/orgmode",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("orgmode")
+
+      vim.keymap.set("n", "<leader>r", require("telescope").extensions.orgmode.refile_heading)
+      vim.keymap.set("n", "<leader>fh", require("telescope").extensions.orgmode.search_headings)
+      vim.keymap.set("n", "<leader>li", require("telescope").extensions.orgmode.insert_link)
+    end,
+  }
 }
