067f102662099d9e6842026f722d46623dc86ce6
Dmitry V. Luciv <dluciv@dluciv.name>
2025-05-09 21:51:36 +0300

    neowarrior

--- private_dot_config/nvim/lua/plugins/plugin_neowarrior.lua
+++ private_dot_config/nvim/lua/plugins/plugin_neowarrior.lua
@@ -1,10 +1,32 @@
 return {
   'duckdm/neowarrior.nvim',
+  event = 'VeryLazy',
   dependencies = {
     'nvim-telescope/telescope.nvim',
     --- Optional but recommended for nicer inputs
     --- 'folke/noice.nvim',
   },
-  --- See config example below
-  opts = {}
+  config = function()
+
+    local nw = require('neowarrior')
+    local home = vim.env.HOME
+    nw.setup({
+      report = "next",
+      filter = "\\(due.before:2d or due: \\)",
+      dir_setup = {
+        {
+          dir = home .. "/dev/nvim/neowarrior.nvim",
+          filter = "project:neowarrior",
+          mode = "tree",
+          expanded = true,
+        },
+      }
+    })
+    vim.keymap.set("n", "<leader>nl", function() nw.open_left() end, { desc = "Open nwarrior on the left side" })
+    vim.keymap.set("n", "<leader>nc", function() nw.open_current() end, { desc = "Open nwarrior below current buffer" })
+    vim.keymap.set("n", "<leader>nb", function() nw.open_below() end, { desc = "Open nwarrior below current buffer" })
+    vim.keymap.set("n", "<leader>na", function() nw.open_above() end, { desc = "Open nwarrior above current buffer" })
+    vim.keymap.set("n", "<leader>nr", function() nw.open_right() end, { desc = "Open nwarrior on the right side" })
+    vim.keymap.set("n", "<leader>nt", function() nw.focus() end, { desc = "Focus nwarrior" })
+  end
 }
