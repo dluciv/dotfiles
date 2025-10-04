b9632c8c19ea0ecedb7dc0635681141f25a051e6
Dmitry V. Luciv <dluciv@dluciv.name>
2025-10-04 13:46:02 +0300

    hiddify

--- private_dot_local/bin/executable_dot_x.autoexec.bat
+++ private_dot_local/bin/executable_dot_x.autoexec.bat
@@ -54,7 +54,8 @@ if [[ $HOST != "clover" ]]; then ionice -c 3 -- nice flameshot &|; fi
 command -v syncthingtray-qt6 &>/dev/null && SYNCTHINGTRAY=syncthingtray-qt6 || SYNCTHINGTRAY=syncthingtray
 { sleep 45; ionice -c 3 -- nice $SYNCTHINGTRAY --wait } &|
 { sleep 5;  nice ~/_/safe_wf/tools/pytraycharmap/run.sh cccp-p &>/dev/null } &|
-{ sleep 5;  nice nekoray &>/dev/null } &|
 
+# { sleep 5;  nice nekoray &>/dev/null } &|
+{ sleep 5;  nice hiddify &>/dev/null } &|
 
 echo `date`: $USER : $0 end >> /tmp/_.xwx.log
