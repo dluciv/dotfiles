fad4919e1e0632337410ca289e133fd076e0dbaf
Dmitry V. Luciv <dluciv@dluciv.name>
2026-06-06 13:39:47 +0300

    clifm

---
+++ dot_zsh/dot_d/55.clifm.rc.d
@@ -0,0 +1,7 @@
+cfm () {
+  clifm --no-restore-last-path --cd-on-quit "$@"
+  local dir="$(grep "^\*" "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm/.last" 2>/dev/null | cut -d':' -f2)";
+  if [ -d "$dir" ]; then
+    cd -- "$dir" || return 1
+  fi
+}
