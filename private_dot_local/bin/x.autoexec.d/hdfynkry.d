a888917901f05d2800091f9e49d74c2939349b2d
Dmitry V. Luciv <dluciv@dluciv.name>
2026-07-13 11:22:36 +0300

    kr

--- private_dot_local/bin/x.autoexec.d/hdfynkry.d
+++ private_dot_local/bin/x.autoexec.d/hdfynkry.d
@@ -1,6 +1,7 @@
 {
   sleep $(( RANDOM % 5 + 1 ));
-  if   nice hiddify &>/dev/null; then
+  if   nice karing  &>/dev/null; then
+  elif nice hiddify &>/dev/null; then
   elif nice nekobox &>/dev/null; then
   elif nice nekoray &>/dev/null; then
   else run-hiddify-cli start; fi
